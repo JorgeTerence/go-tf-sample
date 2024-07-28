a9cc4951361474e02f53fa262708f3616a562276
Jorge Terence <jorge.terence@outlook.com>
2024-07-28 12:39:59 -0300

    created azure resources

--- main.tf
+++ main.tf
@@ -1,8 +1,61 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.46.0"
+      version = "~> 3.0.2"
     }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "go-tf-jt-resources"
+  location = "brazilsouth"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name = "go-tf-jt-zipdeploy"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type = "Linux"
+  sku_name = "F1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name = "go-tf-jt-instance"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id = azurerm_service_plan.sp.id
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    always_on = false
+    
+    application_stack {
+      docker_image = "golang"
+      docker_image_tag = "latest"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "go-tf-jt-vnet"
+  # provider = azurerm.virtual_network
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.0.0.0/16"]
+
+  subnet {
+    name = "go-tf-jt-subnet"
+    address_prefix = "10.0.1.0/24"
   }
 }
